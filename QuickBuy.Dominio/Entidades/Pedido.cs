1fa0b278ef23817bc696a74ef523165df645d6a2
chieffixgo <40673059+chieffixgo@users.noreply.github.com>
2019-05-30 17:33:36 -0300

    Entidades iniciais defeinidas.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public string UsuarioId { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um pedido 
+        /// ou muitos pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        
+        /// <summary>
+        ///Um usuario pode ter nunhum ou muitos pedidos.
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
