af144f10e8f1311fae238691dd956d111ceb8fcf
chieffixgo <40673059+chieffixgo@users.noreply.github.com>
2019-05-30 18:07:13 -0300

    Terminando a criação das entidades.

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,10 +10,19 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public string UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
 
         /// <summary>
-        /// Pedido deve ter pelo menos um pedido 
-        /// ou muitos pedidos
+        /// Pedido deve ter pelo menos um item de pedido 
+        /// ou muitos intens de pedidos
         /// </summary>
         public ICollection<ItemPedido> ItensPedidos { get; set; }
     }
